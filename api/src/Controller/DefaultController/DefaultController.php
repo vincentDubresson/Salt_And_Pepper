504de051c9badd550aa3bce0898d9fcaffbcb0ee
vincentDubresson <vincent.dubresson@orange.fr>
2023-08-26 18:23:56 +0200

    Basic installs

---
+++ api/src/Controller/DefaultController/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\DefaultController;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse('This is the default page of the API', 200);
+    }
+}
