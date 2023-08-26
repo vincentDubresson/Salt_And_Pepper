697a39bf8e8bbf82f6c027448fa5e018471f69e5
vincentDubresson <vincent.dubresson@orange.fr>
2023-08-26 19:38:37 +0200

    PHPCSFIXER : Manual Fix

--- api/src/Controller/DefaultController/DefaultController.php
+++ api/src/Controller/DefaultController/DefaultController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class DefaultController extends AbstractController
 {
     #[Route('/')]
