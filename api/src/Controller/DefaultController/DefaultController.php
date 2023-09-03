3dfde0dac48a463dbf8f8e789f39319a13e733d7
vincentDubresson <vincent.dubresson@orange.fr>
2023-09-03 18:45:30 +0200

    Add twig + create first page

--- api/src/Controller/DefaultController/DefaultController.php
+++ api/src/Controller/DefaultController/DefaultController.php
@@ -4,13 +4,21 @@ namespace App\Controller\DefaultController;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
     {
-        return new JsonResponse('This is the default page of the API', 200);
+        return $this->render('Default/default.html.twig');
+    }
+
+    #[Route('/form-submit', name: 'form_submit', methods: 'POST')]
+    public function formSubmit(Request $request): void
+    {
+        var_dump($request); die;
     }
 }
