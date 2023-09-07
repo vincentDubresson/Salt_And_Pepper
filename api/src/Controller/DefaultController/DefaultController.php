d8b52a5dc992662cf1a104cc006c652e769aad79
vincentDubresson <vincent.dubresson@orange.fr>
2023-09-07 15:47:47 +0200

    Delete useless route in DefaultController.php

--- api/src/Controller/DefaultController/DefaultController.php
+++ api/src/Controller/DefaultController/DefaultController.php
@@ -3,8 +3,6 @@
 namespace App\Controller\DefaultController;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,11 +12,5 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('Default/default.html.twig');
-    }
-
-    #[Route('/form-submit', name: 'form_submit', methods: 'POST')]
-    public function formSubmit(Request $request): void
-    {
-        var_dump($request); die;
     }
 }
