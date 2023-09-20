84b3abc41661a82cd1ee983f2ed213739e7685e3
vincentDubresson <vincent.dubresson@orange.fr>
2023-09-20 19:31:59 +0200

    Install and config of Vich Uploader + Add imagefile in User entity.

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -10,13 +10,16 @@ use Knp\DoctrineBehaviors\Contract\Entity\TimestampableInterface;
 use Knp\DoctrineBehaviors\Model\Timestampable\TimestampableTrait;
 use Symfony\Bridge\Doctrine\Types\UuidType;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[Vich\Uploadable]
 #[UniqueEntity(fields: ['email'], message: 'Cette adresse email a déjà été utilisée.')]
 #[ApiResource(
     // Display when reading the object
@@ -123,6 +126,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Timesta
     #[Assert\Type("\DateTimeInterface")]
     #[Groups(['read', 'write'])]
     private ?\DateTimeImmutable $birthDate = null;
+
+    #[Vich\UploadableField(mapping: 'user', fileNameProperty: 'imageName')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?string $imageName = null;
 
     #[ORM\Column]
     private ?bool $isEnable = null;
@@ -360,6 +369,32 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, Timesta
         $this->phoneNumber = $phoneNumber;
 
         return $this;
+    }
+
+    public function setImageFile(File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): void
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
     }
 
     public function isIsEnable(): ?bool
