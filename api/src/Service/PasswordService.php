dc42ff4b492f870d7e84a3ac737e3da5d63115a3
vincentDubresson <vincent.dubresson@orange.fr>
2023-10-16 19:26:44 +0200

    - Updated User to support creating a new user on the front side - Create a method in PasswordService.php to support password hashing. - Update of UserRepository.php

--- api/src/Service/PasswordService.php
+++ api/src/Service/PasswordService.php
@@ -2,10 +2,33 @@
 
 namespace App\Service;
 
-class PasswordService
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use Exception;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class PasswordService implements ProcessorInterface
 {
+    public function __construct(private readonly ProcessorInterface $processor, private readonly UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
+    public function process($data, Operation $operation, array $uriVariables = [], array $context = [])
+    {
+        if (!$data->getPlainPassword()) {
+            return $this->processor->process($data, $operation, $uriVariables, $context);
+        }
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $data,
+            $data->getPlainPassword()
+        );
+        $data->setPassword($hashedPassword);
+        $data->eraseCredentials();
+        return $this->processor->process($data, $operation, $uriVariables, $context);
+    }
+
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function generatePassword(int $length): string
     {
