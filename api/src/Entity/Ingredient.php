c4fcab2d0628c0077d7e626680a7184c473f6ed2
vincentDubresson <vincent.dubresson@orange.fr>
2023-10-25 20:33:23 +0200

    PHP STAN

--- api/src/Entity/Ingredient.php
+++ api/src/Entity/Ingredient.php
@@ -55,7 +55,7 @@ class Ingredient implements TimestampableInterface, SluggableInterface
 
     public function __toString(): string
     {
-        return $this->getlabel();
+        return (string) $this->getlabel();
     }
 
     public function getId(): ?Uuid
