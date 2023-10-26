a0cf8e9539ab407ba408dc111579d446d5519b1a
vincentDubresson <vincent.dubresson@orange.fr>
2023-10-26 19:34:22 +0200

    - Add and Config Unity Admin

---
+++ api/src/Admin/UnityAdmin.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class UnityAdmin extends AbstractAdmin
+{
+    private TranslatorInterface $translator;
+
+    public function __construct(
+        TranslatorInterface $translator,
+        string $code = null,
+        string $class = null,
+        string $baseControllerName = null
+    ) {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->translator = $translator;
+    }
+
+    public function configure(): void
+    {
+        $this->classnameLabel = $this->translator->trans('sonata_admin.breadcrum.unity_list');
+    }
+
+    protected function configureFormFields(FormMapper $form): void
+    {
+        $form
+            ->with('UnityInfo', [
+                'label' => 'sonata_admin.form.tab_label.unity_info',
+            ])
+                ->add('label', TextType::class, [
+                    'label' => 'sonata_admin.label.general.label',
+                ])
+                ->add('abreviation', TextType::class, [
+                    'label' => 'sonata_admin.label.unity.abreviation',
+                ])
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter): void
+    {
+        $filter->add('label', null, [
+            'label' => 'sonata_admin.label.general.label',
+        ]);
+    }
+
+    protected function configureListFields(ListMapper $list): void
+    {
+        $list
+            ->add('label', null, [
+                'label' => 'sonata_admin.label.general.label',
+            ])
+            ->add('abreviation', null, [
+                'label' => 'sonata_admin.label.unity.abreviation',
+            ])
+            ->add('createdAt', 'date', [
+                'label' => 'sonata_admin.label.general.created_at',
+                'format' => 'd/m/Y - H:i:s',
+                'locale' => 'fr',
+                'timezone' => 'Europe/Paris',
+            ])
+            ->add('updatedAt', 'date', [
+                'label' => 'sonata_admin.label.general.updated_at',
+                'format' => 'd/m/Y - H:i:s',
+                'locale' => 'fr',
+                'timezone' => 'Europe/Paris',
+            ])
+            ->add(ListMapper::NAME_ACTIONS, null, [
+                'label' => 'sonata_admin.general.actions',
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $show): void
+    {
+        $show
+            ->with('ingredientInfo', [
+                'label' => 'sonata_admin.form.tab_label.category_info',
+            ])
+                ->add('label', null, [
+                    'label' => 'sonata_admin.label.general.label',
+                ])
+                ->add('abreviation', null, [
+                    'label' => 'sonata_admin.label.unity.abreviation',
+                ])
+                ->add('createdAt', 'date', [
+                    'label' => 'sonata_admin.label.general.created_at',
+                    'format' => 'd/m/Y - H:i:s',
+                    'locale' => 'fr',
+                    'timezone' => 'Europe/Paris',
+                ])
+                ->add('updatedAt', 'date', [
+                    'label' => 'sonata_admin.label.general.updated_at',
+                    'format' => 'd/m/Y - H:i:s',
+                    'locale' => 'fr',
+                    'timezone' => 'Europe/Paris',
+                ])
+            ->end()
+        ;
+    }
+}
