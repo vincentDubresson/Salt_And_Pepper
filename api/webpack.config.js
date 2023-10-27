3c6b4d33eda09466304a9abb18ef6340f2442ce8
vincentDubresson <vincent.dubresson@orange.fr>
2023-10-27 20:19:47 +0200

    - Install tailwindcss

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -56,6 +56,9 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    // enables PostCSS Loader
+    .enablePostCssLoader()
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
