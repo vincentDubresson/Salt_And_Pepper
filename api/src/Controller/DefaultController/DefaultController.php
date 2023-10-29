a63a783715576e6d412c2abaea1226bcab4f8c7f
vincentDubresson <vincent.dubresson@orange.fr>
2023-10-29 18:59:31 +0100

    add logout in security.yaml

--- api/src/Controller/DefaultController/DefaultController.php
+++ api/src/Controller/DefaultController/DefaultController.php
@@ -13,4 +13,11 @@ class DefaultController extends AbstractController
     {
         return new JsonResponse('This is the default page of the API', 200);
     }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
