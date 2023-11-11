6297d83f90c7cf66ea80381f4c9b8a004af61e4d
vincentDubresson <vincent.dubresson@orange.fr>
2023-11-11 20:24:31 +0100

    Manage header css

--- web-app/src/components/Header/Header.tsx
+++ web-app/src/components/Header/Header.tsx
@@ -1,59 +1,53 @@
 import { Fragment, useState } from 'react';
 import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react';
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from '@heroicons/react/24/outline';
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-  LockClosedIcon,
-} from '@heroicons/react/20/solid';
+import { Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon, LockClosedIcon } from '@heroicons/react/20/solid';
+import BakeryDiningOutlinedIcon from '@mui/icons-material/BakeryDiningOutlined';
+import RiceBowlOutlinedIcon from '@mui/icons-material/RiceBowlOutlined';
+import LocalBarOutlinedIcon from '@mui/icons-material/LocalBarOutlined';
+import DinnerDiningOutlinedIcon from '@mui/icons-material/DinnerDiningOutlined';
+import CakeOutlinedIcon from '@mui/icons-material/CakeOutlined';
+import LiquorOutlinedIcon from '@mui/icons-material/LiquorOutlined';
 
 import MainLogo from '../../../public/pictures/logo/salt_and_pepper_logo.png';
 
 const products = [
   {
-    name: 'Analytics',
-    description: 'Get a better understanding of your traffic',
+    name: 'Apéritifs',
+    description: 'Éveillez les papilles en une ou plusieurs bouchées',
     href: '#',
-    icon: ChartPieIcon,
+    icon: LocalBarOutlinedIcon,
   },
   {
-    name: 'Engagement',
-    description: 'Speak directly to your customers',
+    name: 'Entrées',
+    description: "Découvrez l'explosion de saveurs dès l'entrée",
     href: '#',
-    icon: CursorArrowRaysIcon,
+    icon: RiceBowlOutlinedIcon,
   },
   {
-    name: 'Security',
-    description: 'Your customers’ data will be safe and secure',
+    name: 'Plats',
+    description: 'Succombez à la gourmandise à chaque bouchée',
     href: '#',
-    icon: FingerPrintIcon,
+    icon: DinnerDiningOutlinedIcon,
   },
   {
-    name: 'Integrations',
-    description: 'Connect with third-party tools',
+    name: 'Desserts',
+    description: 'Plaisirs sucrés à savourer délicatement',
     href: '#',
-    icon: SquaresPlusIcon,
+    icon: CakeOutlinedIcon,
   },
   {
-    name: 'Automations',
-    description: 'Build strategic funnels that will convert',
+    name: 'Boissons',
+    description: 'Dégustez nos délices, chauds ou froids',
     href: '#',
-    icon: ArrowPathIcon,
+    icon: LiquorOutlinedIcon,
   },
-];
-
-const callsToAction = [
-  { name: 'Watch demo', href: '#', icon: PlayCircleIcon },
-  { name: 'Contact sales', href: '#', icon: PhoneIcon },
+  {
+    name: 'Petits déjeuners et brunchs',
+    description: 'Réveillez-vous en douceur, démarrez en saveurs',
+    href: '#',
+    icon: BakeryDiningOutlinedIcon,
+  },
 ];
 
 function classNames(...classes: string[]) {
@@ -86,7 +80,7 @@ export default function Header() {
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3BottomRightIcon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
         <div className="hidden sm:flex sm:flex-col sm:w-3/4 lg:w-auto gap-y-3">
@@ -104,8 +98,8 @@ export default function Header() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
                   d="M14.795 13.408l5.204 5.204a1 1 0 01-1.414 1.414l-5.204-5.204a7.5 7.5 0 111.414-1.414zM8.5 14A5.5 5.5 0 103 8.5 5.506 5.506 0 008.5 14z"
                 />
               </svg>
@@ -130,47 +124,32 @@ export default function Header() {
                 leaveFrom="opacity-100 translate-y-0"
                 leaveTo="opacity-0 translate-y-1"
               >
-                <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                  <div className="p-4">
+                <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-gray-900/5">
+                  <div className="flex flex-col p-4 gap-y-2">
                     {products.map((item) => (
                       <div
                         key={item.name}
-                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                        className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 bg-gray-50 hover:bg-sp-primary-100 transition-colors"
                       >
-                        <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-sp-primary-100 group-hover:bg-white transition-colors shadow-sm">
                           <item.icon
-                            className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                            className="h-6 w-6 text-gray-600 group-hover:text-sp-primary-400 transition-colors"
                             aria-hidden="true"
                           />
                         </div>
                         <div className="flex-auto">
                           <a
                             href={item.href}
-                            className="block font-semibold text-gray-900"
+                            className="block font-semibold text-gray-500"
                           >
                             {item.name}
                             <span className="absolute inset-0" />
                           </a>
-                          <p className="mt-1 text-gray-600">
+                          <p className="mt-1 text-gray-400">
                             {item.description}
                           </p>
                         </div>
                       </div>
-                    ))}
-                  </div>
-                  <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                    {callsToAction.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                      >
-                        <item.icon
-                          className="h-5 w-5 flex-none text-gray-400"
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </a>
                     ))}
                   </div>
                 </Popover.Panel>
@@ -187,13 +166,13 @@ export default function Header() {
               href="#"
               className="text-md font-semibold leading-6 text-gray-500 hover:text-sp-primary-400 transition-colors"
             >
-              Boissons
+              Cave à vin
             </a>
             <a
               href="#"
               className="text-md font-semibold leading-6 text-gray-500 hover:text-sp-primary-400 transition-colors"
             >
-              Contact
+              Recette au hasard
             </a>
           </Popover.Group>
         </div>
@@ -201,7 +180,7 @@ export default function Header() {
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <button className="bg-white hover:bg-sp-primary-400 transition-colors text-sp-primary-500 hover:text-gray-50 border-2 border-sp-primary-400 font-bold py-2 px-4 gap-3 rounded-full inline-flex items-center">
             <LockClosedIcon className="h-5 w-5" aria-hidden="true" />
-            <span>Se connecter</span>
+            <span>Connexion</span>
           </button>
         </div>
       </nav>
@@ -211,17 +190,9 @@ export default function Header() {
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        {/* <div className="fixed inset-0 z-10" /> */}
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Salt & Pepper</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+            <div></div>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -233,11 +204,32 @@ export default function Header() {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="lg:hidden relative flex my-6">
+                <input
+                  className="w-full py-2 px-4 border border-gray-300 rounded-full shadow-sm focus:outline-none"
+                  type="search"
+                  placeholder="Chercher une recette..."
+                />
+                <button className="absolute inset-y-0 right-0 flex items-center px-4 text-gray-700 bg-sp-primary-400 border hover:bg-sp-primary-300 transition-colors border-gray-300 rounded-r-full hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500">
+                  <svg
+                    className="h-5 w-5 text-gray-50"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M14.795 13.408l5.204 5.204a1 1 0 01-1.414 1.414l-5.204-5.204a7.5 7.5 0 111.414-1.414zM8.5 14A5.5 5.5 0 103 8.5 5.506 5.506 0 008.5 14z"
+                    />
+                  </svg>
+                </button>
+              </div>
               <div className="space-y-2 py-6">
                 <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
                     <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-500 hover:bg-sp-primary-100">
                         Recettes
                         <ChevronDownIcon
                           className={classNames(
@@ -248,12 +240,12 @@ export default function Header() {
                         />
                       </Disclosure.Button>
                       <Disclosure.Panel className="mt-2 space-y-2">
-                        {[...products, ...callsToAction].map((item) => (
+                        {[...products].map((item) => (
                           <Disclosure.Button
                             key={item.name}
                             as="a"
                             href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-500 hover:bg-sp-primary-100"
                           >
                             {item.name}
                           </Disclosure.Button>
@@ -264,27 +256,27 @@ export default function Header() {
                 </Disclosure>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-500 hover:bg-sp-primary-100"
                 >
                   Les bases
                 </a>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-500 hover:bg-sp-primary-100"
                 >
-                  Boissons
+                  Cave à vin
                 </a>
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-500 hover:bg-sp-primary-100"
                 >
-                  Contact
+                  Recette au hasard
                 </a>
               </div>
               <div className="py-6">
                 <a
                   href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-500 hover:bg-sp-primary-100"
                 >
                   Se connecter
                 </a>
